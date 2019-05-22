1eed0a687e9c978ae6de82fc39aacdb06624f9a8
wintop001 <davidcdx@163.com>
2019-05-22 15:41:22 +0800

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user  
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user  
       # redirect_to user_url(user)
       # flash[:success] = "Welcome!"
     else
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
@@ -15,4 +17,20 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
+  # 登入指定的用户
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+end
+
+class ActionDispatch::IntegrationTest
+  
+  # 登入指定的用户
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+  
 end
