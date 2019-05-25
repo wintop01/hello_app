dd2edf92a34193702c724bff9f7565556cbfca96
wintop001 <davidcdx@163.com>
2019-05-25 13:14:44 +0800

    register by using email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
+      @user.send_activation_email
       flash[:success] = "Welcome to the Sample App"
       redirect_to @user
     else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
 
   test "Invalid signup information" do
     get signup_path
@@ -20,18 +23,36 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User",
-                                email: "user@example.com",
-                                password: "password",
-                                password_confirmation: "password" } }
-    end    
+                                        email: "user@example.com",
+                                        password: "password",
+                                        password_confirmation: "password" } }
+    end
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+    # 尝试在激活之前登录
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    # 激活令牌无效
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+
+    # 令牌有效，电子邮件地址不对
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    
+    # 激活令牌有效
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
-    # assert_select "div", "Welcome to the Sample App"
-    assert_not flash.empty?
     assert is_logged_in?
   end
 
